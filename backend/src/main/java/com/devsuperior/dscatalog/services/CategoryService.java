cc2108773c4ea597071e36aea60a037b81a5ab3c
luizrepiso@yahoo.es <90225213+luizrepiso@users.noreply.github.com>
2021-09-19 21:31:04 -0300

    POST new Category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -13,7 +13,6 @@ import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 
-
 @Service
 public class CategoryService {
 
@@ -25,12 +24,20 @@ public class CategoryService {
 		List<Category> list = repository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
- 
+
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
-	Optional<Category>obj = repository.findById(id);
-	Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not Found"));
-	return new CategoryDTO (entity);
-	
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not Found"));
+		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CategoryDTO(entity);
+
 	}
 }
