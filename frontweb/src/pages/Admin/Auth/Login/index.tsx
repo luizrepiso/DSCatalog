6bd8dcaf0c1b4a2fd7b4a66c039a22e9b567d159
Luiz Repiso <luizrepiso@yahoo.es>
2022-04-13 22:40:37 -0300

    Redirecting programmaticaly

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 
 import { useForm } from 'react-hook-form';
@@ -18,6 +18,8 @@ const [hasError, setHasError] = useState(false);
 
   const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
 
+  const history = useHistory();
+
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
       .then((response) => {
@@ -26,6 +28,7 @@ const [hasError, setHasError] = useState(false);
         console.log('TOKEN GERADO: ' + token);
           setHasError(false);
         console.log('SUCESSO', response);
+        history.push('/admin');
       })
       .catch((error) => {
         setHasError(true);
