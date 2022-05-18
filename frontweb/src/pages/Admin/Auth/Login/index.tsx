4839dab06419f4cb051aa36f9ee4d413a5ff8d1c
Luiz Repiso <luizrepiso@yahoo.es>
2022-05-18 09:12:59 -0300

    hasAnyRoles

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -4,7 +4,7 @@ import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
 import { useLocation } from 'react-router-dom';
 import './styles.css';
-import {getTokenData, requestBackendLogin, saveAuthData } from 'util/requests';
+import { getTokenData, requestBackendLogin, saveAuthData } from 'util/requests';
 import { useContext, useState } from 'react';
 import { AuthContext } from 'AuthContext';
 
@@ -15,19 +15,22 @@ type FormData = {
 
 type LocationState = {
   from: string;
-}
+};
 
 const Login = () => {
-
   const location = useLocation<LocationState>();
 
-  const { from } = location.state || { from: { pathname: 'admin'} }
+  const { from } = location.state || { from: { pathname: 'admin' } };
 
-  const {setAuthContextData} = useContext(AuthContext);
+  const { setAuthContextData } = useContext(AuthContext);
 
-const [hasError, setHasError] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
-  const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const history = useHistory();
 
@@ -35,12 +38,11 @@ const [hasError, setHasError] = useState(false);
     requestBackendLogin(formData)
       .then((response) => {
         saveAuthData(response.data);
-       setHasError(false);
-   setAuthContextData({
-    authenticated: true,
-    tokenData: getTokenData(),
-
-   })
+        setHasError(false);
+        setAuthContextData({
+          authenticated: true,
+          tokenData: getTokenData(),
+        });
 
         history.replace(from);
       })
@@ -54,38 +56,44 @@ const [hasError, setHasError] = useState(false);
     <div className="base-card login-card">
       <h1>LOGIN</h1>
       {hasError && (
-      <div className="alert alert-danger">
-         Erro ao tentar efetuar login
-          </div>
+        <div className="alert alert-danger">Erro ao tentar efetuar login</div>
       )}
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
           <input
             {...register('username', {
               required: 'Campo obrigatório',
-              pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              message: 'Email inválido'
-            }
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Email inválido',
+              },
             })}
             type="text"
-            className={`form-control base-input ${errors.username? 'is-invalid': ''}`}
+            className={`form-control base-input ${
+              errors.username ? 'is-invalid' : ''
+            }`}
             placeholder="Email"
             name="username"
           />
-          <div className="invalid-feedback d-block">{errors.username?.message}</div>
+          <div className="invalid-feedback d-block">
+            {errors.username?.message}
+          </div>
         </div>
         <div className="mb-2">
           <input
             {...register('password', {
-            required: 'Campo obrigatório'
-          })}
-            
+              required: 'Campo obrigatório',
+            })}
             type="password"
-            className={`form-control base-input ${errors.password? 'is-invalid': ''}`}
+            className={`form-control base-input ${
+              errors.password ? 'is-invalid' : ''
+            }`}
             placeholder="Password"
             name="password"
           />
-          <div className="invalid-feedback d-block">{errors.password?.message}</div>
+          <div className="invalid-feedback d-block">
+            {errors.password?.message}
+          </div>
         </div>
         <Link to="/admin/auth/recover" className="login-link-recover">
           Esqueci a senha
