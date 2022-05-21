130864e141d0868643d7a46f731c362b5f50da8a
Luiz Repiso <luizrepiso@yahoo.es>
2022-05-20 23:11:25 -0300

    CRUD  routes

--- frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontweb/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -3,6 +3,7 @@ import ProductPrice from 'components/ProductPrice';
 import CategoryBadge from '../CategoryBadge';
 
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 type Props = {
   product: Product;
@@ -29,9 +30,11 @@ const ProductCrudCard = ({ product }: Props) => {
           <button className="btn btn-outline-danger product-crud-card-button-first">
             EXCLUIR
           </button>
+          <Link to={`admin/products/${product.id}`}>
           <button className="btn btn-outline-secondary product-crud-card-button">
             EDITAR
            </button>
+           </Link>
       </div>
     </div>
   );
